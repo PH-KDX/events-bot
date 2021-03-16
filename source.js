52b25c291a145c8128883506572d5924abbd4083
PH-KDX <smtp.python.email.sender@gmail.com>
2021-03-16 18:30:03 +0100

    general cleanup, added config file

--- source.js
+++ source.js
@@ -1,40 +1,46 @@
-const Discord = require('discord.js')
-const CryptoJS = require('crypto-js')
-const fs = require('fs')
-const client = new Discord.Client()
-var users = []
-var codes = []
-var pics = null
-var removing = null
+const Discord = require('discord.js');
+const CryptoJS = require('crypto-js');
+const fs = require('fs');
+require('dotenv').config()
+
+const mydata = JSON.parse(fs.readFileSync('config.json'));
+const currentelection = mydata['currentelection'];
+
+const client = new Discord.Client();
+
+const users = [];
+const codes = [];
+let pics = null;
+let removing = null;
 
 // define the functions
 
 function convertMiliseconds(miliseconds) {
-	var days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
 
-	total_seconds = parseInt(Math.floor(miliseconds / 1000));
-	total_minutes = parseInt(Math.floor(total_seconds / 60));
-	total_hours = parseInt(Math.floor(total_minutes / 60));
-	days = parseInt(Math.floor(total_hours / 24));
+	const total_seconds = parseInt(Math.floor(miliseconds / 1000));
+	const total_minutes = parseInt(Math.floor(total_seconds / 60));
+	const total_hours = parseInt(Math.floor(total_minutes / 60));
+	const days = parseInt(Math.floor(total_hours / 24));
 
-	seconds = parseInt(total_seconds % 60);
-	minutes = parseInt(total_minutes % 60);
-	hours = parseInt(total_hours % 24);
+	const seconds = parseInt(total_seconds % 60);
+	const minutes = parseInt(total_minutes % 60);
+	const hours = parseInt(total_hours % 24);
 
 
 	return {
 		d: days,
 		h: hours,
 		m: minutes,
-		s: seconds
+		s: seconds,
 	};
 }
 
 function avatar(msg) {
+	let user
 	if (msg.mentions.users.first() == undefined) {
-		var user = msg.author
+		user = msg.author
 	} else {
-		var user = msg.mentions.users.first()
+		user = msg.mentions.users.first()
 	}
 	const avatarEmbed = new Discord.MessageEmbed()
 	avatarEmbed.setColor(0x333333)
@@ -42,28 +48,28 @@ function avatar(msg) {
 	avatarEmbed.setImage(user.displayAvatarURL({
 		format: 'png',
 		dynamic: true,
-		size: 2048
+		size: 2048,
 	}));
 	msg.channel.send(avatarEmbed);
 	return
 }
 
 function codeMsg(msg) {
-	var code = randomString()
+	let code = randomString()
 	if (codes.includes(code)) {
 		code = randomString()
 	}
 	const sendPic = pics[Math.floor(Math.random() * pics.length)];
-	var sendcode = '```' + code + '```'
-	msg1 = `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
+	const sendcode = '```' + code + '```'
+	const msg1 = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 	msg.author.send(msg1)
 	msg.author.send(sendPic)
-	return code
+	return sendcode
 }
 
 function getDateObject(existed) {
-	data = convertMiliseconds(existed)
-	const calculateTimimg = d => {
+	const data = convertMiliseconds(existed)
+	const calculateTiming = d => {
 		let months = 0,
 			years = 0,
 			days = 0,
@@ -82,16 +88,16 @@ function getDateObject(existed) {
 				days++
 				d--
 			}
-		};
+		}
 		return {
 			years,
 			months,
 			weeks,
-			days
+			days,
 		}
 	}
 
-	moredata = calculateTimimg(data.d)
+	const moredata = calculateTiming(data.d)
 
 	return {
 		y: moredata.years,
@@ -100,12 +106,12 @@ function getDateObject(existed) {
 		d: moredata.days,
 		h: data.h,
 		mi: data.m,
-		s: data.s
+		s: data.s,
 	}
 }
 
 function timeHandler(existed) {
-	dateobj = getDateObject(existed)
+	const dateobj = getDateObject(existed)
 
 	if (dateobj.y > 0) {
 		return `Years: ${dateobj.y}, Months: ${dateobj.m}, Weeks: ${dateobj.w}, Days: ${dateobj.d}`
@@ -133,19 +139,19 @@ function help(msg, canvote) {
 	embed.setAuthor(msg.author.tag)
 	embed.addFields({
 		name: '\n$ping',
-		value: 'Gets the latency from message sending to message repsonse'
+		value: 'Gets the latency from message sending to message repsonse',
 	}, {
 		name: '$av',
-		value: "Gets the avatar of the mentioned user if they're in the server. Defaults to the person sending the msg."
+		value: "Gets the avatar of the mentioned user if they're in the server. Defaults to the person sending the msg.",
 	}, {
 		name: '$membercount',
-		value: 'Replies with the number of members in the server.'
+		value: 'Replies with the number of members in the server.',
 	}, {
 		name: '$electioninfo',
-		value: 'Returns an embed with the infomation of all present and upcoming elections.'
+		value: 'Returns an embed with the infomation of all present and upcoming elections.',
 	})
 	if (canvote) {
-		embed.addField('$code', "Election command. DM's the user the election code, along with the instructions for to vote.")
+		embed.addField('$code', 'Election command. DMs the user the election code, along with the instructions for to vote.')
 	}
 	if (msg.member.roles.cache.some(role => role.name === 'Bot mod')) {
 		embed.addField('$approve', 'Gives a user in the waiting room the Junior Pilot role and sends a welcome message.')
@@ -165,17 +171,18 @@ function electioninfo(msg) {
 	embed.setAuthor(msg.author.tag)
 	embed.addFields({
 		name: 'Currently',
-		value: 'There are no current running elections'
+		value: 'There are no current running elections',
 	}, {
 		name: 'Future',
-		value: "There will be a Event Manager election on April 3rd. Get in your flights and you'll be able to run!\nFurther details coming soon."
+		value: `There will be a Event Manager election on April 3rd. Get in your flights and you'll be able to run!
+		Further details coming soon.`,
 	})
 
 	msg.channel.send(embed)
 }
 
 function logger(txt, log = false) {
-	var date = new Date(Date.now()).toUTCString()
+	const date = new Date(Date.now()).toUTCString()
 	fs.appendFileSync('log.txt', `\n${date}: ${txt}`)
 	if (log) {
 		console.log(txt)
@@ -188,18 +195,18 @@ function addImage(link) {
 }
 
 function randomString(length = 64) {
-	var result = '';
-	for (var i = length; i > 0; --i) {
-		num = Math.floor(Math.random() * (126 - 33 + 1) + 33)
+	let result = '';
+	for (let i = length; i > 0; --i) {
+		const num = Math.floor(Math.random() * (126 - 33 + 1) + 33)
 		result += String.fromCharCode(num)
 	}
 	return result
 }
 
 async function ping(msg) {
-	let sent = await msg.channel.send('Loading...')
+	const sent = await msg.channel.send('Loading...')
 
-	var newmsg = `:ping_pong: Pong!\nLatency is ${sent.createdTimestamp - msg.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ws.ping)} ms`
+	const newmsg = `:ping_pong: Pong!\nLatency is ${sent.createdTimestamp - msg.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ws.ping)} ms`
 	sent.edit(newmsg)
 }
 
@@ -209,17 +216,17 @@ function filter(msg) {
 		return false
 	}
 
-	words = msg.content.split(' ')
+	const words = msg.content.split(' ')
 
-	for (var i = 0; i < words.length; i++) {
-		word = words[i]
+	for (let i = 0; i < words.length; i++) {
+		let word = words[i]
 		word = word.replace(/([^a-zA-z0-9]+)/g, '').toLowerCase()
 		words[i] = word
 	}
 
 	// check if any of the words are in the filter list
 
-	for (var i = 0; i < words.length; i++) {
+	for (let i = 0; i < words.length; i++) {
 		if (removing.includes(words[i])) {
 			return true
 		}
@@ -230,14 +237,14 @@ function filter(msg) {
 }
 
 function readJSON() {
-	var data = fs.readFileSync('json/main.json')
+	let data = fs.readFileSync('json/main.json')
 	data = JSON.parse(data)
 	data.election.users.forEach(function(item) {
-		var decrypted = CryptoJS.AES.decrypt(item, process.env.ENCRYPTION_KEY)
+		const decrypted = CryptoJS.AES.decrypt(item, process.env.ENCRYPTION_KEY)
 		users.push(decrypted.toString(CryptoJS.enc.Utf8))
 	})
 	data.election.codes.forEach(function(item) {
-		var decrypted = CryptoJS.AES.decrypt(item, process.env.ENCRYPTION_KEY)
+		const decrypted = CryptoJS.AES.decrypt(item, process.env.ENCRYPTION_KEY)
 		codes.push(decrypted.toString(CryptoJS.enc.Utf8))
 	})
 	pics = data.images
@@ -246,22 +253,22 @@ function readJSON() {
 }
 
 function saveJSON() {
-	var saving = {
+	const saving = {
 		election: {
 			users: [],
-			codes: []
+			codes: [],
 		},
 		images: [],
-		filterlist: []
+		filterlist: [],
 	}
 	saving.images = pics
 	saving.filterlist = removing
 	users.forEach(function(item) {
-		var encrypted = CryptoJS.AES.encrypt(item, process.env.ENCRYPTION_KEY)
+		const encrypted = CryptoJS.AES.encrypt(item, process.env.ENCRYPTION_KEY)
 		saving.election.users.push(encrypted.toString())
 	})
 	codes.forEach(function(item) {
-		var encrypted = CryptoJS.AES.encrypt(item, process.env.ENCRYPTION_KEY)
+		const encrypted = CryptoJS.AES.encrypt(item, process.env.ENCRYPTION_KEY)
 		saving.election.codes.push(encrypted.toString())
 	})
 	fs.writeFileSync('json/main.json', JSON.stringify(saving))
@@ -278,12 +285,13 @@ function getMemberNumber(msg) {
 client.on('ready', () => {
 	// Set the status
 	client.user.setActivity('$help | Watching the GeoFS Events Server', {
-		type: 'PLAYING'
+		type: 'PLAYING',
 	})
+	console.log('ready to go')
 })
 
 client.on('guildMemberRemove', member => {
-	var embed = new Discord.MessageEmbed()
+	const embed = new Discord.MessageEmbed()
 	embed.setColor('#0099ff')
 	embed.setAuthor('Member Left')
 	embed.setDescription(`${member} ${member.user.tag}`)
@@ -293,10 +301,10 @@ client.on('guildMemberRemove', member => {
 })
 
 client.on('guildMemberAdd', member => {
-	existed = Date.now() - member.user.createdAt
+	let existed = Date.now() - member.user.createdAt
 	existed = timeHandler(existed)
 	member.roles.set(['752701923399958610', '553723645265182720'])
-	ageembed = new Discord.MessageEmbed()
+	const ageembed = new Discord.MessageEmbed()
 	ageembed.setColor('#0099ff')
 	ageembed.setAuthor('Member Joined')
 	ageembed.setDescription(`${member} ${member.user.tag}`)
@@ -312,7 +320,7 @@ client.on('message', async (msg) => {
 		return
 	}
 
-	var del = await filter(msg)
+	const del = await filter(msg)
 
 	if (del) {
 		msg.delete()
@@ -321,7 +329,7 @@ client.on('message', async (msg) => {
 
 	try {
 		if (msg.content == '<@780458120605990954>') {
-			var sendingmsg = 'Hello! My command prefix is `$`\nIf you want to get a list of commands you can run `$help`'
+			const sendingmsg = 'Hello! My command prefix is `$`\nIf you want to get a list of commands you can run `$help`'
 			return msg.channel.send(sendingmsg)
 		}
 	} catch (error) {
@@ -331,7 +339,7 @@ client.on('message', async (msg) => {
 	if (!msg.content.startsWith('$')) {
 		return
 	}
-	var canvote = false
+	const canvote = false
 
 	if (msg.content.startsWith('$electioninfo')) {
 		electioninfo(msg)
@@ -339,9 +347,9 @@ client.on('message', async (msg) => {
 	}
 
 	if (msg.content.startsWith('$addimage')) {
-		if (msg.member.roles.cache.has("766386531681435678")) {
+		if (msg.member.roles.cache.has('766386531681435678')) {
 			if (msg.attachments.size > 0) {
-				var Attachment = (msg.attachments).array()
+				const Attachment = (msg.attachments).array()
 				addImage(Attachment[0].url)
 				await msg.channel.send('Image added. Restarting.')
 				process.exit()
@@ -375,29 +383,34 @@ client.on('message', async (msg) => {
 		return msg.channel.send(`There are ${getMemberNumber(msg)} members in the server.`)
 	}
 	if (msg.content.startsWith('$code')) {
-		return msg.channel.send(`${msg.author}, there is no running election!`)
-
-		if (canvote) {
-			if (users.includes(msg.author.id)) {
-				return msg.channel.send(`<@${msg.author.id}>, you've already gotten a code!`)
+		if (currentelection) {
+			if (canvote) {
+				if (users.includes(msg.author.id)) {
+					return msg.channel.send(`<@${msg.author.id}>, you've already gotten a code!`)
+				}
+				const code = codeMsg(msg)
+				codes.push(code)
+				users.push(msg.author.id)
+				saveJSON()
+				return msg.channel.send(`${msg.author},  check your dm's!`)
+			} else {
+				return msg.channel.send(`${msg.author}, you can't vote right now!`)
 			}
-			code = codeMsg(msg)
-			codes.push(code)
-			users.push(msg.author.id)
-			saveJSON()
-			return msg.channel.send(`${msg.author},  check your dm's!`)
+		} else {
+			return msg.channel.send(`${msg.author}, there is no running election!`)
 		}
-		return msg.channel.send(`${msg.author}, you can't vote right now!`)
+
+
 	}
 	if (msg.content.startsWith('$approve')) {
-		if (msg.member.roles.cache.has("766386531681435678")) {
+		if (msg.member.roles.cache.has('766386531681435678')) {
 			if (msg.mentions.members.first() == undefined) {
 				return msg.channel.send(`<@${msg.author.id}>, you have to mention someone!`)
 			}
-			let member = msg.mentions.members.first()
+			const member = msg.mentions.members.first()
 			if (member.roles.cache.some(role => role.name === 'Security Check')) {
 				member.roles.set(['553723642568114187'])
-				var message = `Welcome to GeoFS Events ${member}!\nWe hope you enjoy your stay!\nPlease make sure you have read <#553929583397961740> and <#553720929063141379>.\nWe organize and host events every day, so make sure to check <#756937922904850442> to keep up on events hosted for that day.\nThere are currently ${getMemberNumber(msg)} people in this server.\nIf you need any help or advice, contact the Elite Crew.\nUse <#818115761843339274> to put your event ideas in and we will do it as soon as possible!\n\nCheck out and subscribe to our channel here: <https://www.youtube.com/channel/UCZhJvrv8C6mb0FXENg6uh2w>`
+				const message = `Welcome to GeoFS Events ${member}!\nWe hope you enjoy your stay!\nPlease make sure you have read <#553929583397961740> and <#553720929063141379>.\nWe organize and host events every day, so make sure to check <#756937922904850442> to keep up on events hosted for that day.\nThere are currently ${getMemberNumber(msg)} people in this server.\nIf you need any help or advice, contact the Elite Crew.\nUse <#818115761843339274> to put your event ideas in and we will do it as soon as possible!\n\nCheck out and subscribe to our channel here: <https://www.youtube.com/channel/UCZhJvrv8C6mb0FXENg6uh2w>`
 				client.channels.cache.get('553718744657035274').send(message)
 				return msg.channel.send('User approved sucessfully')
 			}
@@ -406,18 +419,18 @@ client.on('message', async (msg) => {
 		return msg.channel.send(`<@${msg.author.id}>, you can't run that command!`)
 	}
 	if (msg.content.startsWith('$questioning')) {
-		if (msg.member.roles.cache.has("766386531681435678")) {
+		if (msg.member.roles.cache.has('766386531681435678')) {
 			if (msg.mentions.members.first() == undefined) {
 				return msg.channel.send(`<@${msg.author.id}>, you have to mention someone!`)
 			}
-			let member = msg.mentions.members.first()
-			if (member.roles.cache.has("766386531681435678")) {
-				return msg.channel.send("That user can't be sent to the questioning room!")
+			const member = msg.mentions.members.first()
+			if (member.roles.cache.has('766386531681435678')) {
+				return msg.channel.send('That user can\'t be sent to the questioning room!')
 			} else {
-				let rolenames = []
-				let roles = member.roles.member._roles
+				const rolenames = []
+				const roles = member.roles.member._roles
 				roles.forEach(function(item) {
-					let role = msg.guild.roles.cache.get(item)
+					const role = msg.guild.roles.cache.get(item)
 					rolenames.push(role.name)
 				})
 				client.channels.cache.get('760831152109649940').send(`${member} had roles ${rolenames.join(', ')}`)
@@ -428,15 +441,15 @@ client.on('message', async (msg) => {
 		}
 		return msg.channel.send(`<@${msg.author.id}>, you can't run that command!`)
 	}
-	return msg.channel.send(`Seems like that isn't a command!`)
+	return msg.channel.send("Seems like that isn't a command!")
 })
 
-client.on("error", async (e) => {
+client.on('error', async (e) => {
 	await client.channels.cache.get('815629216372621373').send(`The bot ran into an error and needs to restart. ${e}`)
 	await logger(`ERROR ${e}`, true)
 	process.exit(1)
 })
-client.on("warn", (e) => logger(`WARNING: ${e}`, true))
+client.on('warn', (e) => logger(`WARNING: ${e}`, true))
 // client.on("debug", (e) => logger(`DEBUG: ${e}`)
 
 readJSON()
